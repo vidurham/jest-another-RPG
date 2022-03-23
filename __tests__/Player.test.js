a90556ffc7c8bd6c04e65553ebe496177e1e19d9
Vinny D <vdurham@prideseals.com>
2022-03-23 14:23:35 -0400

    Player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,10 +1,8 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
-
-console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -17,3 +15,22 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 })
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test("gets inventory from player or returns false", () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
